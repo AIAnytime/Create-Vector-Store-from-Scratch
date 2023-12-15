d05ee5104ab2e52f6da7205a92a83cfdc8335765
Janki <77482476+jankigabani@users.noreply.github.com>
2023-12-15 12:38:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,30 @@
-from vector_store import VectorStore
 import numpy as np
+from vector_store import VectorStore
 
-# Create a VectorStore instance
 vector_store = VectorStore()
 
-# Define your sentences
-sentences = [
-    "I eat mango",
-    "mango is my favorite fruit",
-    "mango, apple, oranges are fruits",
-    "fruits are good for health",
-]
-
-# Tokenization and Vocabulary Creation
-vocabulary = set()
-for sentence in sentences:
+def preprocess_and_store_sentence(sentence):
     tokens = sentence.lower().split()
-    vocabulary.update(tokens)
-
-# Assign unique indices to words in the vocabulary
-word_to_index = {word: i for i, word in enumerate(vocabulary)}
-
-# Vectorization
-sentence_vectors = {}
-for sentence in sentences:
-    tokens = sentence.lower().split()
-    vector = np.zeros(len(vocabulary))
+    vector = np.zeros(len(tokens))
     for token in tokens:
-        vector[word_to_index[token]] += 1
-    sentence_vectors[sentence] = vector
-
-# Storing in VectorStore
-for sentence, vector in sentence_vectors.items():
+        vector[tokens.index(token)] += 1
     vector_store.add_vector(sentence, vector)
 
-# Searching for Similarity
-query_sentence = "Mango is the best fruit"
-query_vector = np.zeros(len(vocabulary))
+while True:
+    user_input = input("Enter a sentence (or type 'exit' to quit): ")
+    if user_input.lower() == 'exit':
+        break
+    preprocess_and_store_sentence(user_input)
+
+query_sentence = input("Enter a query sentence: ")
+query_vector = np.zeros(len(query_sentence.split()))
 query_tokens = query_sentence.lower().split()
 for token in query_tokens:
-    if token in word_to_index:
-        query_vector[word_to_index[token]] += 1
+    query_vector[query_tokens.index(token)] += 1
 
-similar_sentences = vector_store.find_similar_vectors(query_vector, num_results=2)
+num_results = int(input("Enter the number of similar sentences to retrieve: "))
+similar_sentences = vector_store.find_similar_vectors(query_vector, num_results=num_results)
 
-# Print similar sentences
 print("Query Sentence:", query_sentence)
 print("Similar Sentences:")
 for sentence, similarity in similar_sentences:
