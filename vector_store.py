db68d4d7082502b73fb29c10329a41304c5b52d1
Janki <77482476+jankigabani@users.noreply.github.com>
2023-12-15 12:34:19 +0530

    Update vector_store.py

--- vector_store.py
+++ vector_store.py
@@ -1,71 +1,36 @@
 import numpy as np
 
-
 class VectorStore:
     def __init__(self):
         self.vector_data = {}  # A dictionary to store vectors
         self.vector_index = {}  # An indexing structure for retrieval
 
     def add_vector(self, vector_id, vector):
-        """
-        Add a vector to the store.
-
-        Args:
-            vector_id (str or int): A unique identifier for the vector.
-            vector (numpy.ndarray): The vector data to be stored.
-        """
         self.vector_data[vector_id] = vector
         self._update_index(vector_id, vector)
 
     def get_vector(self, vector_id):
-        """
-        Retrieve a vector from the store.
-
-        Args:
-            vector_id (str or int): The identifier of the vector to retrieve.
-
-        Returns:
-            numpy.ndarray: The vector data if found, or None if not found.
-        """
         return self.vector_data.get(vector_id)
 
     def _update_index(self, vector_id, vector):
-        """
-        Update the index with the new vector.
-
-        Args:
-            vector_id (str or int): The identifier of the vector.
-            vector (numpy.ndarray): The vector data.
-        """
-        # In this simple example, we use brute-force cosine similarity for indexing
         for existing_id, existing_vector in self.vector_data.items():
-            similarity = np.dot(vector, existing_vector) / (np.linalg.norm(vector) * np.linalg.norm(existing_vector))
+            similarity = self._calculate_cosine_similarity(vector, existing_vector)
             if existing_id not in self.vector_index:
                 self.vector_index[existing_id] = {}
             self.vector_index[existing_id][vector_id] = similarity
 
+    def _calculate_cosine_similarity(self, vector1, vector2):
+        norm1 = np.linalg.norm(vector1)
+        norm2 = np.linalg.norm(vector2)
+        if norm1 == 0 or norm2 == 0:
+            return 0  # Handle division by zero
+        return np.dot(vector1, vector2) / (norm1 * norm2)
+
     def find_similar_vectors(self, query_vector, num_results=5):
-        """
-        Find similar vectors to the query vector using brute-force search.
-
-        Args:
-            query_vector (numpy.ndarray): The query vector for similarity search.
-            num_results (int): The number of similar vectors to return.
-
-        Returns:
-            list: A list of (vector_id, similarity_score) tuples for the most similar vectors.
-        """
         results = []
         for vector_id, vector in self.vector_data.items():
-            similarity = np.dot(query_vector, vector) / (np.linalg.norm(query_vector) * np.linalg.norm(vector))
+            similarity = self._calculate_cosine_similarity(query_vector, vector)
             results.append((vector_id, similarity))
 
-        # Sort by similarity in descending order
         results.sort(key=lambda x: x[1], reverse=True)
-
-        # Return the top N results
         return results[:num_results]
-
-
-
-
